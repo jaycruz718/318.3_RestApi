1a2944abdac15505a793d3481f2da462f128ae11
Tiamat <Jaycruz718@gmail.com>
2025-09-05 09:49:33 -0400

    Continuing yesterday's lecture on RestAPI

--- routes/postRoutes.mjs
+++ routes/postRoutes.mjs
@@ -5,8 +5,27 @@ const router = express.Router();
 // @route GET /api/posts
 // @desc Get all posts
 // @access Public
-router.get("/", (req, res) => {
+router.route("/")
+.get((req, res) => {
   res.json(posts);
+})
+.post((req, res) => {
+  const { userId, title, content } = req.body; // grabbing data the client entered through req.body
+  // let id = posts[posts.length - 1].id + 1; // creating a new id
+  let id = posts.length++;
+
+  // creating a new object that will be pushed to the posts array
+  if (userId && title && content) {
+
+  const post = {
+    id: id,
+    userId: userId,
+    title: title,
+    content: content,
+  };
+  posts.push(post);
+  res.json(post);
+  }
 });
 
 //  @route GET /api/posts/:id
@@ -16,6 +35,13 @@ router.get("/:id", (req, res, next) => {
   const post = posts.find((post) => post.id == req.params.id);
 
   if (post) res.json(post)
+  else next();
+});
+
+router.route("/:id").get((req, res, next) => {
+  const post = posts.find((post) => post.id == req.params.id);
+
+  if (post) res.json(post);
   else next();
 });
 
